4598521e96b5154d771cdd98d19937a98d316f87
Rabot9ga <freedomboxnikita@inbox.ru>
2024-06-28 00:12:42 +0300

    final to show

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ async def get_url(i):
     global lst_of_games
     async with aiohttp.ClientSession() as session:
         async with session.get('https://psprices.com/region-tr/collection/all-discounts?page=' + str(i)) as resp:
+            print(i)
             soup = BeautifulSoup(await resp.text(), 'html.parser')
             lst_of_games.extend([i['href'] for i in soup.find_all('a',
                     'flex flex-col gap-1 relative mt-1 z-10 focus:ring rounded text-text group')])
@@ -20,12 +21,33 @@ async def http_test(url):
             print(resp.status)
 
 
-async def main():
+async def async_main():
     global lst_of_games
     await asyncio.gather(*(get_url(i) for i in range(1, 4)))
     await asyncio.gather(*(http_test(i) for i in lst_of_games))
 
+
+
+def get_urls_games():
+    global lst_of_games
+    for i in range(1, 4):
+        response = requests.get('https://psprices.com/region-tr/collection/all-discounts?page=' + str(i))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        lst_of_games.extend([i['href'] for i in soup.find_all('a',
+                                                              'flex flex-col gap-1 relative mt-1 z-10 focus:ring rounded text-text group')])
+        print(i)
+def get_respons_stat():
+    global lst_of_games
+    for i in lst_of_games:
+        print(requests.get('https://psprices.com'+i).status_code)
+
+
+def main():
+    get_urls_games()
+    get_respons_stat()
+
 if __name__ == "__main__":
     start = time.time()
-    asyncio.run(main())
+    asyncio.run(async_main())
+    # main()
     print(time.time()-start)
